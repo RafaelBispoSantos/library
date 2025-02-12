ed076d38334283cf7652c9e3270e17eabc054a6d
Rafael Bispo <rafael_bispo23@hotmail.com>
2025-02-12 20:26:08 -0300

    create Home Layout, hoem Data,

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+ images:{
+  remotePatterns: [
+    {
+      protocol: "https",
+      hostname: "phacehold.co",
+      
+    },
+  ],
+  domains: ["m.media-amazon.com"],
+ }
 };
 
 export default nextConfig;
---
+++ types.d.ts
@@ -0,0 +1,15 @@
+interface Book {
+  id: number;
+  title: string;
+  autor: string;
+  genre: string;
+  rating: number;
+  total_copies: number;
+  available_copies: string;
+  description: string;
+  color: string;
+  cover: string;
+  video: string;
+  summart: string;
+  isLoanedBook: boolean;
+}
